37204fc3920d3d1ac8eb1f2391accafc8fb374ee
hungpm372 <hungpm372@gmail.com>
2024-06-26 20:18:58 +0700

    Create chat bot translate

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import os
-
 from dotenv import load_dotenv
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
 load_dotenv(dotenv_path=".env.dev")
 
 model = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-messages = [
-    SystemMessage(content="Translate the following from Vietnamese to English"),
-    HumanMessage(content="Xin chào, tôi tên là Huy. Tôi đến từ Việt Nam."),
-]
+system_template = "You are a helpful assistant that can translate between Vietnamese and English."
+human_template = "{text}"
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", human_template)]
+)
 
 parser = StrOutputParser()
 
-chain = model | parser
+chain = prompt_template | model | parser
 
-output = chain.invoke(messages)
-
-print(output)
+while True:
+    user_input = input("Human: ")
+    if user_input.lower() == "exit":
+        break
+    output = chain.invoke({"text": user_input})
+    print(f"Assistant: {output}")
